d6bfe3c1c98443690fcb719b16bfb1a1bb34313b
Taygun Savaş <taygunsavas@windowslive.com>
2022-12-01 15:57:32 +0300

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    
+    private void LateUpdate()
+    {
+  
+    }
+}
